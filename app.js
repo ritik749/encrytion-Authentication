94a08e1006d77ea5f74ae28c8b4bdc234bc1d52c
Ritik Kumar <ritik070201@gmail.com>
2021-09-12 15:31:20 +0530

    level-4 Hashing

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyparser = require("body-parser");
 const ejs= require("ejs");
 const app = express();
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 app.set("view engine","ejs");
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static("Public"));
@@ -14,7 +14,7 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema =  new mongoose.Schema({email: String, password: String });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+
 
 const User = mongoose.model("User",userSchema);
 
@@ -34,7 +34,7 @@ app.get("/login", function(req, res){
 app.post("/register", function(req,res){
     const user = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     user.save(function(err){
@@ -47,7 +47,7 @@ app.post("/register", function(req,res){
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     
     User.findOne({email: username},function(err, founduser){
         if(!err)
